ff6ea3edcb0c5735ef41b8b2e1403efc1babcef6
Duberly2004 <ivanmondragonmanchay@gmail.com>
2023-08-16 11:43:14 -0500

    add route base

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {Routes,Route} from 'react-router-dom'
 import Header from "./components/layout/Header";
-import './app.css';
+import './App.css';
 export default function App() {
   return (
     <>
