0c49589298e60eb46b4a6881e1d74c42b3304dd5
Duberly2004 <ivanmondragonmanchay@gmail.com>
2023-08-25 18:12:46 -0500

    change profile

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -12,14 +12,14 @@ export default function ProfilePage() {
     <div className="flex flex-col items-center mx-auto">
       <div className="mb-6 mt-16 w-full flex flex-col items-center mx-auto">
         <div className="bg-blue-600 color-text text-center w-32 h-32 rounded-full border flex items-center justify-center overflow-hidden">
-          <img src={ronilProfile} alt="" />
-          {/* {user.picture == null ? (
+          {user.picture == null ? (
             <h1 className="text-5xl text-white">{user.name.charAt(0)}</h1>
           ) : (
             <img src={user.picture} alt="Perfile del usuario"/> //Agregar foto de perfil aqui
-          )} */}
+          )}
         </div>
       </div>
+      {/* Text profile */}
       <div className="text-center border bg-white w-full h-full color-text font-bold py-4">
         <h2 className="text-2xl">{`${user.paternal_surname} ${user.maternal_surname} ${user.name}`}</h2>
         <h2 className="color-text-primary my-2">{TextRole(user.role.name)}</h2>
